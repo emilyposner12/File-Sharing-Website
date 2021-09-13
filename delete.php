89ebd3c805375503209bd9d7ee4bbb98acea0ccd
ehedden <ethan.hedden@wustl.edu>
2021-09-13 10:29:01 -0500

    changes

--- delete.php
+++ delete.php
@@ -5,6 +5,8 @@
     
     $fullpath = sprintf('/home/ehedden/fileSharingUsers/%s/%s', $username, $file_name);
     unlink($fullpath);
-    header("Location: usermainpage.php");
+    echo("File deleted.");
+    echo("<br>");
+    echo"<a href='usermainpage.php'> Return to File Selection Screen </a>";
     exit;
 ?>
